257e5a889a82f724911dabf0796184d44fd34d17
Shazzad Hasan <shazzadraihan@gmail.com>
2022-06-14 21:25:08 +0600

    updated helper_plot, show_example added

--- LeNet-5/helper_plot.py
+++ LeNet-5/helper_plot.py
@@ -2,7 +2,25 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt 
 
-# visualize a sample test results
+
+def show_examples(train_loader):
+    # obtain one batch of training images
+    dataiter = iter(train_loader)
+    images, labels = dataiter.next()
+    images = images.numpy()
+    
+    # plot the images in the batch, along with the corresponding labels
+    fig = plt.figure(figsize=(25, 4))
+    for idx in np.arange(20):
+        ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+        ax.imshow(np.squeeze(images[idx]), cmap='gray')
+        # print out the correct label for each image
+        # .item() gets the value contained in a Tensor
+        ax.set_title(str(labels[idx].item()))
+    plt.show()
+        
+    
+
 def show_sample_test_result(test_loader, model, classes, device):
     # get one batch of test images
     dataiter = iter(test_loader)
@@ -23,3 +41,4 @@ def show_sample_test_result(test_loader, model, classes, device):
         ax.imshow(inputs[idx])
         ax.set_title("{} ({})".format(classes[predicted_labels[idx]], classes[targets[idx]]),
                      color=("green" if predicted_labels[idx]==targets[idx].item() else "red"))  
+    plt.show()

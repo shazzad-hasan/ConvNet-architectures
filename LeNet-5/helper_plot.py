f294eed63c7692805bebd6aea42aebe8066d7402
Shazzad Hasan <shazzadraihan@gmail.com>
2022-06-19 15:15:18 +0600

    updated helper_plot.py

--- LeNet-5/helper_plot.py
+++ LeNet-5/helper_plot.py
@@ -2,21 +2,21 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt 
 
+def imshow(img):
+  plt.imshow(np.squeeze(img), cmap='gray')
 
 def show_examples(train_loader):
     # obtain one batch of training images
     dataiter = iter(train_loader)
-    images, labels = dataiter.next()
-    images = images.numpy()
+    inputs, targets = dataiter.next()
+    inputs = inputs.numpy()
     
     # plot the images in the batch, along with the corresponding labels
-    fig = plt.figure(figsize=(25, 4))
-    for idx in np.arange(20):
-        ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
-        ax.imshow(np.squeeze(images[idx]), cmap='gray')
-        # print out the correct label for each image
-        # .item() gets the value contained in a Tensor
-        ax.set_title(str(labels[idx].item()))
+    fig = plt.figure(figsize=(10, 4))
+    for idx in np.arange(10):
+        ax = fig.add_subplot(2, 10/2, idx+1, xticks=[], yticks=[])
+        imshow(inputs[idx])
+        ax.set_title(str(targets[idx].item()))
     plt.show()
         
 
@@ -49,7 +49,6 @@ def show_sample_test_result(test_loader, model, classes, train_on_gpu, device):
     dataiter = iter(test_loader)
     inputs, targets = dataiter.next()
     inputs.numpy()
-    
     inputs = inputs.to(device)
     
     # get sample outputs
@@ -58,12 +57,11 @@ def show_sample_test_result(test_loader, model, classes, train_on_gpu, device):
     _, preds_tensor = torch.max(outputs, 1)
     predictions = np.squeeze(preds_tensor.numpy()) if not train_on_gpu else np.squeeze(preds_tensor.cpu().numpy())
     
-    
     # plot the images in the batch along with predicted and true labels
     fig = plt.figure(figsize=(25, 4))
     for idx in np.arange(20):
         ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
-        ax.imshow(np.squeeze(inputs[idx]) if not train_on_gpu else np.squeeze(inputs[idx].cpu()), cmap="gray")
+        imshow(inputs[idx] if not train_on_gpu else inputs[idx].cpu())
         ax.set_title("{} ({})".format(classes[predictions[idx]], classes[targets[idx]]),
                      color=("green" if predictions[idx]==targets[idx].item() else "red"))
     plt.show()
